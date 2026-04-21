c6842fa1be5caaf87a554a056d419d63c0e747a3
tanamarugithuber <daichitan0223@gmail.com>
2026-04-21 19:09:11 +0900

    make .bat

---
+++ .bat
@@ -0,0 +1,10 @@
+rm *.mod
+rm *.exe
+rm *.o  
+gfortran -c constant_mod.f90
+gfortran -c nucleus_mod.f90
+gfortran -c grid_mod.f90
+gfortran -c CG_method_mod.f90
+gfortran -c frldm_mod.f90
+gfortran -c main.f90
+gfortran constant_mod.o nucleus_mod.o grid_mod.o CG_method_mod.o frldm_mod.o main.o -o main.exe
