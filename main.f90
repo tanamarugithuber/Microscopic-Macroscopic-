736db62c29b3b8489c68b14c87227f759dd8550a
tanamarugithuber <daichitan0223@gmail.com>
2026-04-21 18:13:21 +0900

    Update main.f90

--- main.f90
+++ main.f90
@@ -16,19 +16,19 @@ program main
     nucleus%A = 8
     nucleus%Z = 8
     nucleus%semi1 = nucleus%R0*nucleus%A**(1.0_dp/3.0_dp)
-    call calculate_fundamental_properties(nucleus)
+    call nucleus%calculate_fundamental_properties()
 
     ! Initialize grid
     g_mod%h_x = 0.4_dp
     g_mod%h_y = 0.4_dp
     g_mod%h_z = 0.4_dp
     g_mod%n_times = 3
-    call initialize_grid(g_mod, nucleus)
+    call g_mod%initialize_grid(nucleus)
     allocate(g_mod%density_index(g_mod%n_points))
-    call inside_outside_nucleus(g_mod, nucleus)
+    call g_mod%inside_outside_nucleus(nucleus, g_mod%density_index)
 
     ! Calculate FRLDM variables
-    call calculate_frldm_variables(frldm, nucleus, g_mod)
+    call frldm%calculate_frldm_energy(nucleus, g_mod, frldm%E_frldm)
 
     print *, "FRLDM Energy: ", frldm%E_frldm
 
