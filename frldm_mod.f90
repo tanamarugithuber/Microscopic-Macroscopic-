f17100a611aa110fd973d8d6af6fb296509a6221
tanamarugithuber <daichitan0223@gmail.com>
2026-04-21 16:52:51 +0900

    Update frldm_mod.f90

--- frldm_mod.f90
+++ frldm_mod.f90
@@ -65,29 +65,89 @@ module frldm_mod
         real(dp) :: B_s
         real(dp) :: B_3
         real(dp) :: B_1
-        real(dp), allocatable :: B_1pot(:)
-        real(dp), allocatable :: B_3pot(:)
+        ! yukawa potential 
+        real(dp), allocatable :: B_1pot_y(:)
+        real(dp), allocatable :: B_3pot_y(:)
+        ! coulomb potential
+        real(dp), allocatable :: B_3pot_c(:)
+        ! exponential factors for the surface and Coulomb energy
+        real(dp), allocatable :: B_1exp(:)
+        real(dp), allocatable :: B_3exp(:)
+
         real(dp) :: B_w
         real(dp) :: E_frldm
 
         contains
             procedure :: calculate_frldm_energy
             procedure :: calculate_b_1_b_3
+            procedure :: calculate_exp
     end type frldm_variables
 
     public :: frldm_variables
 
     contains
+        subroutine calculate_exp(this, nucleus, g_mod)
+            class(frldm_variables), intent(inout) :: this
+            type(nucleus_property), intent(in) :: nucleus
+            type(grid_type), intent(in) :: g_mod
+
+            integer :: i, j, k, l, n_max
+            real(dp) :: a_Yukawa_inv = 1.0_dp / a_Yukawa
+            real(dp) :: a_den_inv = 1.0_dp / a_den
+            real(dp) :: x, y, z
+            n_max = g_mod%n_points
+
+
+            allocate(this%B_1exp(n_max))
+            allocate(this%B_3exp(n_max))
+            do k = 1, g_mod%n_z_points
+                z = (g_mod%n_z_min + k - 1) * g_mod%h_z
+                do j = 1, g_mod%n_y_points
+                    y = (g_mod%n_y_min + j - 1) * g_mod%h_y
+                    do i = 1, g_mod%n_x_points
+                        x = (g_mod%n_x_min + i - 1) * g_mod%h_x
+                        l = (k-1)*g_mod%n_x_points*g_mod%n_y_points + (j-1)*g_mod%n_x_points + i
+                        this%B_1exp(l) = exp(- (x**2 + y**2 + z**2) * a_Yukawa_inv)
+                        this%B_3exp(l) = exp(- (x**2 + y**2 + z**2) * a_den_inv)
+                    end do
+                end do
+            end do
+        end subroutine calculate_exp
+
+        subroutine calculate_b_1_b_3(this, nucleus,g_mod)
+            class(frldm_variables), intent(inout) :: this
+            type(nucleus_property), intent(in) :: nucleus
+            type(grid_type), intent(in) :: g_mod
+            
+            integer :: i
+            integer :: n_max
+            real(dp) :: denominator1 
+            real(dp) :: denominator3
+            real(dp) :: inv_a_den = 1.0_dp / a_den
+
+            denominator1 = 8.0_dp * pi**2 * nucleus%R0**2 * a_Yukawa**4
+            denominator3 = 32.0_dp * pi**2 * nucleus%R0**5
+            allocate(this%B_1pot_y(g_mod%n_points))
+            allocate(this%B_3pot_y(g_mod%n_points))
+            allocate(this%B_3pot_c(g_mod%n_points))
+            do i = 1, g_mod%n_points
+                this%B_1 = this%B_1 + g_mod%density_index(i) *(2*a_Yukawa * this%B_1pot_y(i) - this%B_1exp(i)) * g_mod%dV
+                this%B_3 = this%B_3 + g_mod%density_index(i) * (this%B_3pot_c(i) - this%B_3pot_y(i) - 0.5_dp*this%B_3exp(i)*inv_a_den) * g_mod%dV
+            end do
+            this%B_1 = this%B_1 * real(nucleus%A, dp)**(-2.0_dp / 3.0_dp)/ denominator1
+            this%B_3 = this%B_3 * 15.0_dp * real(nucleus%A, dp)**(-5.0_dp / 3.0_dp)/ denominator3
+
+
+
+
+        end subroutine calculate_b_1_b_3
+
         subroutine calculate_frldm_energy(this, nucleus, g_mod, E_frldm)
             class(frldm_variables), intent(inout) :: this
             type(nucleus_property), intent(in) :: nucleus
             type(grid_type), intent(in) :: g_mod
             real(dp), intent(out) :: E_frldm
             real(dp) :: E_v, E_s, E_c, E_cec, E_pffc, E_ca, E_W, E_pair, E_be
-            allocate(this%B_1pot(g_mod%n_x_points * g_mod%n_y_points * g_mod%n_z_points))
-            allocate(this%B_3pot(g_mod%n_x_points * g_mod%n_y_points * g_mod%n_z_points))
-
-
             !----------------------------
             ! clculate the FRLDM variables
             !----------------------------
@@ -111,7 +171,7 @@ module frldm_mod
 
             this%B_w = 1.0_dp
             
-            call this%calculate_b_1_b_3(nucleus,g_mod, this%B_1, this%B_3)
+            call this%calculate_b_1_b_3(nucleus,g_mod)
 
             !----------------------------
             ! volume energy
@@ -175,40 +235,5 @@ module frldm_mod
             E_frldm = E_v + E_s + E_c + E_cec + E_pffc + E_ca + E_W + E_pair + E_be
 
         end subroutine calculate_frldm_energy
-
-
-
-        subroutine calculate_b_1_b_3(this, nucleus,g_mod, B_1, B_3)
-            class(frldm_variables), intent(inout) :: this
-            type(nucleus_property), intent(in) :: nucleus
-            type(grid_type), intent(in) :: g_mod
-            real(dp), intent(out) :: B_1, B_3
-            
-            integer :: i
-            integer :: n_max
-            n_max = g_mod%n_x_points * g_mod%n_y_points * g_mod%n_z_points
-            
-
-            do i = 1, n_max
-                B_1 = B_1 + this%B_1pot(i) * g_mod%dV
-                B_3 = B_3 + this%B_3pot(i) * g_mod%dV
-            end do
-            
-            ! B_1 = B_1* real(nucleus%A, dp)**(-2.0_dp / 3.0_dp)
-            ! B_3 = B_3* real(nucleus%A, dp)**(-2.0_dp / 3.0_dp)
-
-
-            
-
-            !----------------------------
-            ! Calculate the integrands for B_1 and B_3
-            !----------------------------
-
-
-
-        end subroutine calculate_b_1_b_3
-
-
-
-
+        
 end module frldm_mod
